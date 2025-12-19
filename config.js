c10d45822641f43c6049a23e70046ec187431831
Tall Labs <ally.labriola@gmail.com>
2025-12-19 21:37:44 +1300

    Update config.js

--- config.js
+++ config.js
@@ -3,16 +3,13 @@ window.PaywallConfig = {
   cookieDomain: "thesigmundproject.org",
 
   stripe: {
-    individualPlanUrl: "https://buy.stripe.com/YOUR_INDIVIDUAL_LINK",
-    agencyPlanUrl: "https://buy.stripe.com/YOUR_AGENCY_LINK",
+    individualPlanUrl: "https://buy.stripe.com/14AbJ22hI9r6dom5OX4AU01",
+    agencyPlanUrl: "https://buy.stripe.com/7sY8wQ3lM9r63NM1yH4AU00",
     successParam: "paywall=success"
   },
 
   access: {
     storageKey: "sigmund_paywall_access",
-    allowlistEmails: [
-      // paste existing MemberSpace subscriber emails here
-      "user@example.com"
-    ]
+    allowlistEmails: []
   }
 };
